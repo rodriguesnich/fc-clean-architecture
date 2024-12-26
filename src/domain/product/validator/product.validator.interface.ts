c87122db8b2968d85631d3b378903bc8379b8ce7
Nicholas Rodrigues <rodriguesnich@outlook.com>
2024-12-26 18:29:48 -0300

    add notifcation pattern to products entity

---
+++ src/domain/product/validator/product.validator.interface.ts
@@ -0,0 +1,4 @@
+import ValidatorInterface from "../../@shared/validator/validator.interface";
+import Product from "../entity/product";
+
+export default interface ProductValidatorInterface extends ValidatorInterface<Product> {}
